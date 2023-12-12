e73c17370f1b238f1faf1a2cd597c98f1506450a
Ekaterina <kvotua.job@gmail.com>
2023-12-12 22:16:09 +0200

    added link for channel

--- create_channel.py
+++ create_channel.py
@@ -1,36 +1,55 @@
-from pyrogram import Client
-from pyrogram.handlers import MessageHandler
-from pyrogram.raw import functions, types
-from dotenv import load_dotenv
 import os
+import json
+
+from dotenv import load_dotenv
+
+from pyrogram import Client
+from pyrogram.raw.functions.messages import Search
+from pyrogram.raw.types import InputPeerSelf, InputMessagesFilterEmpty
+from pyrogram.raw.types.messages import ChannelMessages
 
 load_dotenv(dotenv_path='.env')
 
 api_id = os.getenv('api_id')
 api_hash = os.getenv('api_hash')
+channel_kvotua = os.getenv('test_channels')
+channel_id = os.getenv('test_channel_id')
 
-user_auth = Client(
+client = Client(
     'my_account',
     api_id=api_id,
     api_hash=api_hash
 )
 
-def my_function(client, message):
-    print(message.text)
+with client:
+    client.send_sticker(
+    chat_id="me", 
+    sticker="CAACAgIAAxkBAAEoNxBleGTk8ZnvZwO7H7mvCP5AaWs89gACwRgAAn2IqUiOzhz_SUBYkDME"),
+    about_me = client.get_me()
+    about_skyfox = client.get_users('skyfox1994')
 
 
-my_handler = MessageHandler(my_function)
-user_auth.add_handler(my_handler)
+file1 = open("data_output/about_me.json", "w") 
+file1.write(str(about_me))
 
-user_auth.run()
+file2 = open("data_output/about_skyfox.json", "w") 
+file2.write(str(about_skyfox))
 
-# with user_auth:
-#     user_auth.send(
+file1.close()
+file2.close()
+
+with client:
+    kvotua_channel = client.get_chat(channel_kvotua)
+    link1 = (client.create_chat_invite_link(chat_id=channel_kvotua, name='Давай дружить!')).invite_link
+    client.send_message('skyfox1994', link1)
+
+# with client:
+#     client.send(
 #         functions.channels.InviteToChannel(
-#             channel=user_auth.resolve_peer("kyruso"), # ID or Username
+#             channel=client.resolve_peer("kyruso"), # ID or Username
 #             users=[ # The users you want to invite
-#                 user_auth.resolve_peer("skyfox1994"), # By username
-#                 user_auth.resolve_peer("andreymaltc"), # By phone number
+#                 client.resolve_peer("skyfox1994"), # By username
+#                 client.resolve_peer("andreymaltc"), # By phone number
 #             ]
 #         )
 #     )
