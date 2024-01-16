2c3f846d0b611dd67ab24ec389e8943329eb12f9
Ekaterina <kvotua.job@gmail.com>
2024-01-16 13:24:34 +0200

    added read write settings data sheets

--- bot/sheets.py
+++ bot/sheets.py
@@ -13,8 +13,37 @@ credentials = ServiceAccountCredentials.from_json_keyfile_name(
 )
 httpAuth = credentials.authorize(httplib2.Http())
 service = googleapiclient.discovery.build("sheets", "v4", http=httpAuth)
+
 sheet_rows = 2
 last_row = 0
+sheet_id = 0
+sheet_count = 1
+max_sheets = 200
+
+
+def read_data():
+    f = open("sheet_data.txt", "r")
+    read_str = f.readlines()
+    temp = []
+    for i in read_str:
+        temp.append(i.split("=")[1].replace("\n", ""))
+    spreadsheetId = temp[0]
+    sheet_rows = temp[1]
+    last_row = temp[2]
+    sheet_id = temp[3]
+    sheet_count = temp[4]
+
+
+def write_data():
+    f = open("sheet_data.txt", "a+")
+    write_str = f"spreadsheetId={spreadsheetId}\n"
+    write_str += f"sheet_rows={sheet_rows}\n"
+    write_str += f"last_row={last_row}\n"
+    write_str += f"sheet_id={sheet_id}\n"
+    write_str += f"sheet_count={sheet_count}\n"
+    f.writelines(write_str)
+
+
 driveService = googleapiclient.discovery.build("drive", "v3", http=httpAuth)
 access = (
     driveService.permissions()
