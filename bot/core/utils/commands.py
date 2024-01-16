c367c5bbc99a49621fe727c0c6b50be1abc5241a
Ekaterina <kvotua.job@gmail.com>
2024-01-16 16:49:36 +0200

    refactor summ sheets rows

--- bot/core/utils/commands.py
+++ bot/core/utils/commands.py
@@ -1,25 +1,13 @@
 from aiogram import Bot
 from aiogram.types import BotCommand, BotCommandScopeDefault
 
+
 async def set_commands(bot: Bot):
     commands = [
-        BotCommand(
-            command='start',
-            description='Начало работы'
-        ),
-        BotCommand(
-            command='help',
-            description='Помощь'
-        ),
-        BotCommand(
-            command='cancel',
-            description='Сбросить'
-        )
-        ,
-        BotCommand(
-            command='clean',
-            description='Очистить'
-        )
+        BotCommand(command="start", description="Начало работы"),
+        BotCommand(command="help", description="Помощь"),
+        BotCommand(command="cancel", description="Сбросить"),
+        BotCommand(command="sheets", description="Таблица"),
     ]
 
     await bot.set_my_commands(commands, BotCommandScopeDefault())
